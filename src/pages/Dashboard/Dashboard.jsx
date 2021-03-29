69c42664bfbea97b85b093103ab4ea149d8531d9
Khoby-790 <ebaidoo79@gmail.com>
2021-03-29 17:20:43 +0000

    --- Auto-Git Commit ---

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -5,6 +5,12 @@ const Dashboard = () => {
     <Fragment>
       <div className="flex h-full">
         <div className="w-64 border-r h-full bg-gray-200"></div>
+        <div className="flex-1 flex flex-col">
+          <div className="flex w-full justify-between">
+              <div></div>
+              <div className="flex"></div>
+          </div>
+        </div>
       </div>
     </Fragment>
   );

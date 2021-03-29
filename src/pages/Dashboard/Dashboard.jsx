39669d724ae885bfede39d8fb2e1dcdafeac878b
Khoby-790 <ebaidoo79@gmail.com>
2021-03-29 17:21:49 +0000

    --- Auto-Git Commit ---

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -5,10 +5,12 @@ const Dashboard = () => {
     <Fragment>
       <div className="flex h-full">
         <div className="w-64 border-r h-full bg-gray-200"></div>
-        <div className="flex-1 flex flex-col">
-          <div className="flex w-full justify-between">
+        <div className="flex-1 flex flex-col px-3">
+          <div className="flex w-full justify-between py-3">
               <div></div>
-              <div className="flex"></div>
+              <div className="flex">
+                  <button className="bg-indigo-400">Hello</button>
+              </div>
           </div>
         </div>
       </div>

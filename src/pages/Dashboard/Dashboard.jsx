f7b0ca61fa38f2d936a6a486c5a713ea92cafcb8
Khoby-790 <ebaidoo79@gmail.com>
2021-03-29 17:19:38 +0000

    --- Auto-Git Commit ---

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,8 +3,8 @@ import React, { Fragment } from "react";
 const Dashboard = () => {
   return (
     <Fragment>
-      <div className="flex">
-        
+      <div className="flex h-full">
+        <div className="w-64 border-r h-full bg-gray-200"></div>
       </div>
     </Fragment>
   );

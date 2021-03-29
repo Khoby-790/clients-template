1d82d54af93c2e295790f34a9adc413f415f42c5
Khoby-790 <ebaidoo79@gmail.com>
2021-03-29 17:16:23 +0000

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import AppLayout from './layout/App';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
-  return <AppLayout />;
+  return (
+    <BrowserRouter>
+      <AppLayout />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/layout/AppContent.jsx
+++ src/layout/AppContent.jsx
@@ -29,20 +29,7 @@ const AppContent = () => {
           className="flex-1 relative z-0 overflow-y-auto focus:outline-none"
           tabindex="0"
         >
-          <div className="py-6">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              <h1 className="text-2xl font-semibold text-gray-900">
-                Dashboard
-              </h1>
-            </div>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-              {/* <!-- Replace with your content --> */}
-              <div className="py-4">
-                <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
-              </div>
-              {/* <!-- /End replace --> */}
-            </div>
-          </div>
+          
         </main>
       </div>
     </Fragment>
